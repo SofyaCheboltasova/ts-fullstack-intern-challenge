e82bdc2d3f9916bb066210d96af98696b09f3bf0
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-07 18:32:27 +0700

    api/feat: enable cors

--- api/index.ts
+++ api/index.ts
@@ -4,8 +4,13 @@ import initDatabase from '../backend/index';
 
 async function bootstrap() {
   await initDatabase();
-
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(8000);
 }
 bootstrap();
