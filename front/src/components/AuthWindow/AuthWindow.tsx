0f21440d04e6f3170470b53f4132dbae0fbd318c
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-08 00:45:40 +0700

    front/feat: feat Auth form

---
+++ front/src/components/AuthWindow/AuthWindow.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Input from "../Input/Input";
+import style from "./AuthWindow.module.scss";
+
+interface AuthWindowProps {
+  onLogin: (login: string, password: string) => void;
+  onClose: () => void;
+}
+
+export default function AuthWindow(props: AuthWindowProps) {
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [loginError, setLoginError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+
+  function handleLoginSubmit(value: string) {
+    if (value.length < 8) {
+      setLoginError("Логин должен быть не короче 8 символов");
+    } else {
+      setLoginError(null);
+      setLogin(value);
+    }
+  }
+
+  function handlePasswordSubmit(value: string) {
+    if (value.length < 8) {
+      setPasswordError("Пароль должен быть не короче 8 символов");
+    } else {
+      setPasswordError(null);
+      setPassword(value);
+    }
+  }
+
+  async function handleButtonClick() {
+    if (login && password) {
+      props.onClose();
+      props.onLogin(login, password);
+    }
+  }
+
+  return (
+    <>
+      <div className={style.background} onClick={props.onClose}></div>
+
+      <div className={style.auth__wrapper}>
+        <div>Авторизация</div>
+        <div className={style.auth__inputs}>
+          <Input
+            placeholder={"Логин"}
+            type={"text"}
+            onSubmit={handleLoginSubmit}
+            error={loginError !== null}
+          />
+          {loginError && <div className={style.auth__error}>{loginError}</div>}
+
+          <Input
+            placeholder={"Пароль"}
+            type={"password"}
+            onSubmit={handlePasswordSubmit}
+            error={passwordError !== null}
+          />
+          {passwordError && (
+            <div className={style.auth__error}>{passwordError}</div>
+          )}
+        </div>
+        <button className={style.auth__button} onClick={handleButtonClick}>
+          Войти
+        </button>
+      </div>
+    </>
+  );
+}
+
