18bfc0e9643f805e81fbac0308feaee5c42bedf3
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-08 00:46:40 +0700

    front/feat: add user creation request

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -4,10 +4,26 @@ import CatData from "../types/catData";
 export async function fetchCats(page: number): Promise<CatData[] | undefined> {
   try {
     const response = await axios.get("http://localhost:8000/cats?page=" + page);
-    const data: CatData[] = await response.data;
-    return data;
+    return response.data;
   } catch (err) {
     console.error(err);
+  }
+}
+
+export async function createUser(
+  login: string,
+  password: string
+): Promise<string | null> {
+  try {
+    const userData = { login: login, password: password };
+    const response = await axios.post("http://localhost:8000/user", userData, {
+      headers: { "Content-Type": "application/json" },
+    });
+    const token = response.headers["x-auth-token"];
+    return token;
+  } catch (err) {
+    console.error(err);
+    return null;
   }
 }
 
