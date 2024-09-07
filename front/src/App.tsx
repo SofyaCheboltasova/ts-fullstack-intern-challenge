0fecf0b8211e292eae06c869de8a3e2bf3d74206
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-07 18:34:09 +0700

    front/feat: add routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,33 @@
 import style from "./App.module.scss";
 import Gallery from "./components/Gallery/Gallery";
 import Header from "./components/Header/Header";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <section className={style.app}>
-      <Header />
-      <Gallery />
-    </section>
+    <Router>
+      <section className={style.app}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/cats" />} />
+          <Route path="/cats" element={<Gallery />} />
+          <Route path="*" element={<div>Page not found</div>} />
+        </Routes>
+      </section>
+    </Router>
   );
 }
 
 export default App;
+
+/**
+ * Галерею
+ * ЛК : вход выход
+ * Проверка токена на бэке
+ * Дефолтные страница с авторизацией
+ */
