1af0071e88d8035165bcb3724078e739079f1029
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-09 00:00:57 +0700

    front/feat: add logic for liked cats

--- front/src/components/Gallery/Gallery.tsx
+++ front/src/components/Gallery/Gallery.tsx
@@ -1,25 +1,33 @@
 import { RefObject, useEffect, useRef, useState } from "react";
 import style from "./Gallery.module.scss";
 
-import { fetchCats } from "../../api/catsApi";
-import CatData from "../../types/CatType";
+import { fetchCatImage, fetchCats } from "../../api/catsApi";
+import { getLikes } from "../../api/likeApi";
+
 import Block from "../Block/Block";
-import { CatResponse } from "../../types/Response";
+import BlockType from "../../types/BlockType";
+import CatType from "../../types/CatType";
+import { useAuth } from "../../contexts/AuthContext";
 
 interface GalleryProps {
+  type: "all" | "likes";
   scrollRef: RefObject<HTMLDivElement>;
 }
 
-export default function Gallery({ scrollRef }: GalleryProps) {
-  const [allCats, setCats] = useState<CatData[]>([]);
+export default function Gallery({ type, scrollRef }: GalleryProps) {
+  const [blocks, setBlocks] = useState<BlockType[]>([]);
   const [page, setPage] = useState<number>(1);
   const [hasMoreCats, setHasMoreCats] = useState<boolean>(true);
-  const [emptyPage, setEmptyPage] = useState<boolean>(false);
   const loaderRef = useRef<HTMLDivElement>(null);
+  const { isAuthorized } = useAuth();
 
   useEffect(() => {
     getCats();
-  }, [page]);
+  }, [page, type]);
+
+  useEffect(() => {
+    setBlocks([]);
+  }, [type]);
 
   useEffect(() => {
     const handleScroll = async () => {
@@ -37,34 +45,77 @@ export default function Gallery({ scrollRef }: GalleryProps) {
     };
   }, [hasMoreCats, scrollRef]);
 
+  function handleLikeClick(catId: string) {
+    if (type === "likes") {
+      setBlocks((prevBlocks) =>
+        prevBlocks.filter((block) => block.id !== catId)
+      );
+    }
+  }
+
+  function getBlock(id: string, url: string, isLiked: boolean) {
+    let newBlock: BlockType = { id, url, isLiked };
+    return newBlock;
+  }
+
   async function getCats(): Promise<void> {
-    const { cats, error }: CatResponse = await fetchCats(page);
+    const cats: CatType[] = await fetchCats(page);
 
-    if (error) {
-      setEmptyPage(true);
-      setHasMoreCats(false);
+    if (type === "all") {
+      if (!cats.length) {
+        setHasMoreCats(false);
+        return;
+      }
+      setHasMoreCats(cats.length > 100);
+
+      for (const cat of cats) {
+        let isLiked = false;
+        if (isAuthorized) {
+          const likes = await getLikes();
+          isLiked = isAuthorized && likes.some((l) => l.cat_id === cat.id);
+        }
+        const block = getBlock(cat.id, cat.url, isLiked);
+        setBlocks((blocks) => [...blocks, block]);
+      }
     }
 
-    if (cats) {
-      setCats((prevCats) => [...prevCats, ...cats]);
-      setEmptyPage(false);
-    }
-    if (cats && cats.length < 100) {
-      setHasMoreCats(false);
+    if (type === "likes") {
+      const likes = await getLikes();
+
+      if (!likes.length) {
+        setHasMoreCats(false);
+        return;
+      }
+      setHasMoreCats(likes.length > 100);
+
+      for (const like of likes) {
+        const url: string = await fetchCatImage(like.cat_id);
+        const block = getBlock(like.cat_id, url, true);
+        setBlocks((blocks) => [...blocks, block]);
+      }
     }
   }
 
   return (
     <section className={style.gallery}>
       <div className={style.gallery__blocks}>
-        {allCats.map((cat, index) => {
-          return <Block key={index} blockData={cat} />;
+        {blocks.map((block, index) => {
+          return (
+            <Block
+              key={index}
+              data={block}
+              onLikeClick={() => handleLikeClick(block.id)}
+            />
+          );
         })}
       </div>
-      {emptyPage && (
-        <div className={style.gallery_empty}>Извините, котиков не будет :(</div>
+      {hasMoreCats ? (
+        <div ref={loaderRef} className={style.loader}>
+          ... Загружаем еще котиков ...
+        </div>
+      ) : (
+        <div className={style.loader}>Извините, котиков больше нет :(</div>
       )}
-      {hasMoreCats && <div ref={loaderRef}>... Загружаем еще котиков ...</div>}
     </section>
   );
 }
