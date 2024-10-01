a031f4f53c15707f8f4619a34a8d0bfb1facabc8
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-01 20:17:49 +0700

    api/refactor: refactoe database creation & initialization

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module';
-import initDatabase from '../backend/index';
+import { Client } from 'pg';
 
 async function bootstrap() {
-  await initDatabase();
+  await createDatabase();
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080/',
+    origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization, X-User-Id',
     exposedHeaders: ['x-auth-token'],
@@ -15,3 +15,35 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+async function createDatabase() {
+  // const client = new Client({
+  //   host: 'cat-pinterest-api-pg',
+  //   port: 5432,
+  //   user: 'postgres',
+  //   password: '1',
+  // });
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'Sofya2002',
+  });
+  const databaseName = 'support_lk_db';
+
+  try {
+    await client.connect();
+    const result = await client.query(
+      `SELECT 1 FROM pg_database WHERE datname = '${databaseName}'`,
+    );
+
+    if (result.rowCount === 0) {
+      await client.query(`CREATE DATABASE ${databaseName}`);
+      console.log(`Database ${databaseName} created`);
+    }
+  } catch (error) {
+    console.error('Error while creating database:', error);
+  } finally {
+    await client.end();
+  }
+}
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-import LikeController from './controllers/like.controller';
-import UserController from './controllers/user.controller';
-import LikeService from '../../backend/src/service/likeService';
-import UserService from '../../backend/src/service/userService';
-import CatController from './controllers/cat.controller';
-
+import { CatModule } from './cat/cat.module';
+import { LikeModule } from './like/like.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { Like } from './like/like.entity';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
-  controllers: [CatController, LikeController, UserController],
-  providers: [LikeService, UserService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // host: 'cat-pinterest-api-pg',
+      // port: 5432,
+      // username: 'postgres',
+      // password: '1',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Sofya2002',
+      database: 'support_lk_db',
+      synchronize: true,
+      logging: true,
+      entities: [User, Like],
+    }),
+    UserModule,
+    CatModule,
+    LikeModule,
+  ],
 })
 export class AppModule {}
